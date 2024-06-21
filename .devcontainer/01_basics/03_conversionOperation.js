806966803ec5eb2e74dd5858f6f39548eb9f5855
LAV RAMAN SINHA <140974741+Kulax@users.noreply.github.com>
2024-06-21 08:21:28 +0000

    operaions

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -24,7 +24,47 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
 
+//*************************************************** Operations ***************************************************
 
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 ="Hello"
+let str2 = " Lav"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log("");
+
+// console.log(+true); // => 1
+// console.log(+""); // => 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100 
+console.log(gameCounter);
+
+++gameCounter;
+console.log(gameCounter);
+
+gameCounter++;
+console.log(gameCounter);
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
