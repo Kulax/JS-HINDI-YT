98ccec1485dae4462ea0376dd6ccf62d36ab8329
LAV RAMAN SINHA <140974741+Kulax@users.noreply.github.com>
2024-06-21 05:59:40 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // Treat all js code as newer version  or newer js
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    + 3) // code readability should be high
+
+let name = "Lav"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power 53 
+// bigInt 
+// string => ""
+// boolean  => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined);// type - undefined
+console.log(typeof null);// type - object
+
