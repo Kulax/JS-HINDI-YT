9a9e55f7bbbf6f152595f37df8f326fcfd1754bd
LAV RAMAN SINHA <140974741+Kulax@users.noreply.github.com>
2024-06-21 13:54:02 +0000

    comparison

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0); // "=="(equality check) and comparisions i.e; >, <, >=, <= work differently 
+// console.log(null == 0);// where comparisons convert null to number so null > 0 is false and null >= 0 is true
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+// ""==="" for strict check datatype check too with value check and no conversion done while checking
+// console.log("2" === 2);
